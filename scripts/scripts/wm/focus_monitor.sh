0f0c592e78f2f3a28639eb58a7559539d12fc8d0
expectocode <expectocode@gmail.com>
2018-09-19 23:02:30 +0100

    multi monitor focus

--- scripts/scripts/wm/focus_monitor.sh
+++ scripts/scripts/wm/focus_monitor.sh
@@ -4,10 +4,7 @@
 #
 # wm key bindings
 
-bspc query -N -n "$1" > /dev/null || exit
-# exit when nothing in specified direction
-
 # teleport cursor on the focused window
 bspc config pointer_follows_focus true
-bspc node -f "$1"
+bspc monitor -f next
 bspc config pointer_follows_focus false
