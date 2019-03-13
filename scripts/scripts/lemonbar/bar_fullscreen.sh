fb0bc79156cb42ff18ab55165171ef24c26395e2
expectocode <expectocode@gmail.com>
2019-03-13 19:44:12 +0000

    &&

--- scripts/scripts/lemonbar/bar_fullscreen.sh
+++ scripts/scripts/lemonbar/bar_fullscreen.sh
@@ -3,6 +3,6 @@
 mapfile -t panel_ids < <( xdo id -a lemonpanel )
 bspc subscribe node_state| while read _ a b wid action state; do
     for panel_id in "${panel_ids[@]}"; do
-        [[ $action = "fullscreen" -a $state = "on" ]] && xdo below -t $wid $panel_id
+        [[ $action = "fullscreen" && $state = "on" ]] && xdo below -t $wid $panel_id
     done
 done
