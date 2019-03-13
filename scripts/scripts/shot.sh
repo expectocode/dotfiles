0b9db472ea7bb1087543296e875f7aebab20e5c6
expectocode <expectocode@gmail.com>
2019-03-13 19:44:25 +0000

    switch to hacksaw

--- scripts/scripts/shot.sh
+++ scripts/scripts/shot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-if [[ $1 == "slopclip" ]]; then
-	sel=$(slop -f "-i %i -g %g")
-	shotgun $sel - | xclip -t 'image/png' -selection clipboard
+if [[ $1 == "selectclip" ]]; then
+	sel=$(hacksaw)
+	shotgun -g "$sel" - | xclip -t 'image/png' -selection clipboard
 else
 	file=$(mktemp /tmp/XXXXX.png)
 	shotgun "$file" && gimp "$file"
