b404dda888e9fea8a0a10e9c7063ba3aee706192
expectocode <expectocode@gmail.com>
2020-01-15 10:37:58 +0000

    vol: parse pamixer less badly

--- scripts/scripts/voldown.sh
+++ scripts/scripts/voldown.sh
@@ -5,7 +5,7 @@ if (( $# > 0 )); then
     sink=$1
     if [[ $sink == "bt" ]]; then
         # parse pactl to get the Bluetooth sink number
-        sink=$(pamixer --list-sinks | tail -n1 | cut -c1)
+        sink=$(pamixer --list-sinks | tail -n1 | cut -d' ' -f1)
     fi
 else
     sink="$default_sink"
