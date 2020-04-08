258458576edd8fc77acf131689addf726a1610d9
expectocode <expectocode@gmail.com>
2020-04-08 19:31:58 +0100

    Fix play/pause icon for spotify

--- scripts/scripts/lemonbar/music.sh
+++ scripts/scripts/lemonbar/music.sh
@@ -3,6 +3,13 @@
 music(){
 	song=$(mpc current -f "%artist% - %title%")
 
+	mpcstatus=$(mpc status | grep -E "(playing|paused)" -o)
+	if [ "$mpcstatus" == "playing" ]; then
+		icon='\uE034'
+	else
+		icon='\uE037'
+	fi
+
 	if [ -z "$song" ]; then #if empty
 		fil=$(mpc current -f '%file%')
 		song="${fil##*/}"
@@ -11,17 +18,11 @@ music(){
             song=$(for nid in $(bspc query -N -d $(bspc query -D -d '^9') -n .window); do
                 xdotool getwindowname "$nid"
             done | rg '·.+Firefox' | tail -n1 | sed 's/ - Mozilla Firefox//')
+
+            icon='\uE034'
         fi
 	fi
-	# song=$(echo $song | cut -d "(" -f 1) #cut out anything following a (
-	# song+=" - "$(mpc current -f %artist%) #add the - artist bit
 
-	mpcstatus=$(mpc status | grep -E "(playing|paused)" -o)
-	if [ "$mpcstatus" == "playing" ]; then
-		icon='\uE034'
-	else
-		icon='\uE037'
-	fi
 
 	echo -e "%{A:mpc -q toggle:}$icon $song %{A}"
 }
