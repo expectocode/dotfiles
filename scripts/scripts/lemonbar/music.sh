f01355353ff58412a0d96e9b44a21699839e5ee6
expectocode <expectocode@gmail.com>
2020-04-08 18:34:15 +0100

    Add hacky Spotify title detection for music

--- scripts/scripts/lemonbar/music.sh
+++ scripts/scripts/lemonbar/music.sh
@@ -7,6 +7,11 @@ music(){
 		fil=$(mpc current -f '%file%')
 		song="${fil##*/}"
 		# lmao does this work
+        if [[ -z "$song" ]]; then # if still empty
+            song=$(for nid in $(bspc query -N -d $(bspc query -D -d '^9') -n .window); do
+                xdotool getwindowname "$nid"
+            done | rg '·.+Firefox' | tail -n1 | sed 's/ - Mozilla Firefox//')
+        fi
 	fi
 	# song=$(echo $song | cut -d "(" -f 1) #cut out anything following a (
 	# song+=" - "$(mpc current -f %artist%) #add the - artist bit
