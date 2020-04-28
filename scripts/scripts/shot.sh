3b2465e8a0fdfab52589a1f874797b9601ba1703
expectocode <expectocode@gmail.com>
2020-04-28 19:42:09 +0100

    Add freezing screenshot script

--- scripts/scripts/shot.sh
+++ scripts/scripts/shot.sh
@@ -2,6 +2,12 @@
 if [[ $1 == "selectclip" ]]; then
 	sel=$(hacksaw)
 	shotgun -g "$sel" - | xclip -t 'image/png' -selection clipboard
+elif [[ $1 == "freeze" ]]; then
+    shotgun - | pqiv -ic -z 1.000000001 - &
+    pid=$!
+    geom=$(hacksaw)
+    shotgun -g $geom - | xclip -selection clipboard -t "image/png"
+    kill $pid
 else
 	file=$(mktemp /tmp/XXXXX.png)
 	shotgun "$file" && gimp "$file"
