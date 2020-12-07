6b2c5551e92af0896193326a9e5db527d5c8d8fa
expectocode <expectocode@gmail.com>
2020-12-07 17:59:58 +0000

    finally handle fullscreen bar properly
    turns out the trick was lowering the monitor windows. who knew.

--- scripts/scripts/lemonbar/bar_fullscreen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mapfile -t panel_ids < <( xdo id -a lemonpanel )
-bspc subscribe node_state| while read _ a b wid action state; do
-    for panel_id in "${panel_ids[@]}"; do
-        [[ $action = "fullscreen" && $state = "on" ]] && xdo below -t $wid $panel_id
-    done
-done
