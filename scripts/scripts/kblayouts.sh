e8de043821e255365c63aa469ec88d44bfce9320
expectocode <expectocode@gmail.com>
2023-12-06 16:12:59 +0000

    model 100 kblayout

--- scripts/scripts/kblayouts.sh
+++ scripts/scripts/kblayouts.sh
@@ -1,6 +1,9 @@
 atreus_id=$(xinput | rg 'Atreus Keyboard.+keyboard'|cut -f 2|cut -d= -f2)
+model100_id=$(xinput | rg 'Model 100 Keyboard'|cut -f 2|cut -d= -f2)
 moonlander_id=$(xinput | rg 'Moonlander Mark I    .+keyboard'|cut -f 2|cut -d= -f2)
 setxkbmap us -variant dvp -option ctrl:swapcaps -option compose:ralt
 setxkbmap -device "$atreus_id" us -option
 setxkbmap -device "$atreus_id" us -option compose:ralt
+setxkbmap -device "$model100_id" us -option
+setxkbmap -device "$model100_id" us -option compose:ralt
 setxkbmap -device "$moonlander_id" us -option compose:ralt
